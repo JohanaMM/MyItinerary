5512aae39c6e3827b3da26b05dc915e16964c3dd
JohanaMM <89425234+JohanaMM@users.noreply.github.com>
2023-07-02 20:45:01 -0300

    replace image

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,17 +1,15 @@
 import '../styles/Hero.css'
 import { Link as LinkRouter } from 'react-router-dom'
-import logoHero from '../imagenes/mytinerary.png'
+import Navbar from './Navbar'
 
 
 function Hero() {
 
 
     return (
-        <>
+        <div>
+        <Navbar/>
             <div className='pagP'>
-                <div className='Logo'>
-                    <img src={logoHero} className='logoNavbar' alt="" />
-                </div>
                 <div className='C-Texto'>
                     <span className='imgL' />
                     <h1 className='textoC'>Find your perfect trip designed by insiders who know and love their cities</h1>
@@ -20,7 +18,7 @@ function Hero() {
                     </LinkRouter>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
